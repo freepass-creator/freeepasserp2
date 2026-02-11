41af3aca6918f46c7d253daea0ad5f074d1525f2
freepass-creator <dudguq@gmail.com>
2026-02-11 13:42:57 +0900

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -2,13 +2,13 @@
 
 import { createContext, useContext, useState } from "react";
 
-const ToastContext = createContext(null);
+const ToastContext = createContext({ showMessage: () => {} });
 
 export function ToastProvider({ children }) {
   const [msg, setMsg] = useState("");
 
   function showMessage(text) {
-    setMsg(text);
+    setMsg(String(text || ""));
     setTimeout(() => setMsg(""), 3000);
   }
 
@@ -16,7 +16,7 @@ export function ToastProvider({ children }) {
     <ToastContext.Provider value={{ showMessage }}>
       {children}
       {msg && (
-        <div className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-black text-white px-4 py-2 rounded">
+        <div className="fixed bottom-10 left-1/2 -translate-x-1/2 bg-slate-900 text-white px-4 py-2 rounded shadow">
           {msg}
         </div>
       )}
