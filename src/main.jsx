4f7fd7dbfef9b15b11b2482038e9638526a17447
freepass-creator <dudguq@gmail.com>
2026-02-12 20:40:57 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js' // .js 확장자 명시
+import App from './App.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
